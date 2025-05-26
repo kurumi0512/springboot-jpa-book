bf41ba404df8c3b6a9885f222508c23a36dce249
kurumimi <foreverlove0512t@gmail.com>
2025-05-26 23:57:33 +0800

    250526 23:57

--- src/main/java/com/example/demo/repository/BiographyRepository.java
+++ src/main/java/com/example/demo/repository/BiographyRepository.java
@@ -11,13 +11,14 @@ import com.example.demo.model.entity.Biography;
 @Repository
 public interface BiographyRepository extends JpaRepository<Biography, Integer> {
 
-	// @Query("select b from Biography b inner join fetch b.author") // Biography 與
-	// Author 同時都有
+	// @Query("select b from Biography b inner join fetch b.author")
+	// Biography 與Author 同時都有,c會出不來,因為c沒有對應的作者
 	// A 1
 	// B 2
 	// D 4
 
 	@Query("select b from Biography b left join fetch b.author") // 若沒有查到 Author 則該欄位顯示 null
+	// 往左靠,用author拼進去
 	// A 1
 	// B 2
 	// C null
